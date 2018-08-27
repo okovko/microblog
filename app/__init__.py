0632edd7087d7567f3789d212b5325a70ad4dba0
okovko <okovko@gmail.com>
2018-08-27 15:28:07 -0700

    added moment.js to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment, _moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +17,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+app.jinja_env.globals['moment'] = _moment
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
