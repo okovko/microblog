63d23a7afa0de58867998e3559651f6edd8ae04c
okovko <okovko@gmail.com>
2018-09-08 11:18:24 -0700

    flask_moment now uses the locale to display internationalized time

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request
+from flask import g, render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
-from flask_babel import _, lazy_gettext as _l
+from flask_babel import _, lazy_gettext as _l, get_locale
 from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, \
@@ -17,6 +17,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/index', methods = ['GET', 'POST'])
