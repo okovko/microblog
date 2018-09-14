36d947fdadb6a15e8a816c0a46b86d1a645feaad
okovko <okovko@gmail.com>
2018-09-13 21:14:36 -0700

    added search functionality to front end

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_wtf import FlaskForm
 from flask_babel import lazy_gettext as _l
 from wtforms import StringField, SubmitField, TextAreaField
@@ -23,3 +24,13 @@ class PostForm(FlaskForm):
     post = TextAreaField(_l('Say something'), validators = [
         DataRequired(), Length(min = 1, max = 140)])
     submit = SubmitField(_l('Post'))
+
+class SearchForm(FlaskForm):
+    query = StringField(_l('Search'), validators = [DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
--- app/main/routes.py
+++ app/main/routes.py
@@ -9,6 +9,7 @@ from app.main.forms import EditProfileForm, PostForm
 from app.models import User, Post
 from app.translate import translate
 from app.main import bp
+from app.main.forms import SearchForm
 
 def is_absolute_url(next_page):
     return url_parse(next_page).netloc != ''
@@ -18,6 +19,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+        g.search_form = SearchForm()
     g.locale = str(get_locale())
     
 @bp.route('/translate', methods = ['POST'])
@@ -148,3 +150,18 @@ def explore():
             next_url = next_url,
             prev_url = prev_url,
     )
+
+@bp.route('/search')
+@login_required
+def search():
+    if not g.search_form.validate():
+        return
+    page = request.args.get('page', 1, type = int)
+    posts, total = Post.search(g.search_form.query.data, page,
+                               current_app.config['POSTS_PER_PAGE'])
+    next_url = url_for('main.search', query = g.search_form.query.data, page = page + 1) \
+        if total > page * current_app.config['POSTS_PER_PAGE'] else None
+    prev_url = url_for('main.search', query = g.search_form.query.data, page = page - 1) \
+        if page > 1 else None
+    return render_template('search.html', title = _('Search'), posts = posts,
+                           next_url = next_url, prev_url = prev_url)
