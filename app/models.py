aece48f7523ed7d1cafb0835d89df03bf3bda883
okovko <okovko@gmail.com>
2018-10-09 01:07:10 -0700

    added hover pop up functionality to front end

--- app/models.py
+++ app/models.py
@@ -5,8 +5,8 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import current_app
 from flask_login import UserMixin
-import jwt
 from app.search import add_to_index, remove_from_index, query_index
+import jwt, json
 
 class SearchableMixin(object):
     @classmethod
@@ -61,7 +61,9 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default = datetime.utcnow)
+    last_message_read_time = db.Column(db.DateTime)
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+    notifications = db.relationship('Notification', backref = 'user', lazy = 'dynamic')
     followed = db.relationship(
             'User',
             secondary = followers,
@@ -70,6 +72,18 @@ class User(UserMixin, db.Model):
             backref = db.backref('followers', lazy = 'dynamic'),
             lazy = 'dynamic'
             )
+    messages_sent = db.relationship(
+        'Message',
+        foreign_keys = 'Message.sender_id',
+        backref = 'author',
+        lazy = 'dynamic',
+    )
+    messages_received = db.relationship(
+        'Message',
+        foreign_keys = 'Message.recipient_id',
+        backref = 'recipient',
+        lazy = 'dynamic',
+    )
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -110,6 +124,24 @@ class User(UserMixin, db.Model):
                 algorithm = 'HS256'
         ).decode('utf-8')
 
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name = name).delete()
+        notification = Notification(
+            name = name,
+            payload_json = json.dumps(data),
+            user = self,
+        )
+        db.session.add(notification)
+        return notification
+
+    def get_tasks(self, *args, **kwargs):
+        return Task.query.filter_by(*args, user = self, **kwargs).all()
+
+    def get_new_message_count(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient = self).filter(
+            Message.timestamp > last_read_time).count()
+
     @staticmethod
     def verify_password_reset_token(token):
         try:
@@ -132,3 +164,26 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message> {}>'.format(self.body)
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(128), index = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index = True, default = time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
+
+    def __repr__(self):
+        return '<Notification> {}>'.format(self.payload_json)
